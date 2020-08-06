8b6fd3b0cdcf166f10db491e9c92d79fe374f8f5
Odairto <odairto@msn.com>
2020-08-06 18:47:14 -0300

    Creating the project

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PageHeader from '../../components/PageHeader/index';
+
+export default function TeacherForm() {
+    return (
+        <div>
+            return <PageHeader title="Que incrível que você quer dar aulas" />;
+        </div>
+    )
+}
